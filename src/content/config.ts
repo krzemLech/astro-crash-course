3b80728e6d03758145145e8b591c308de7ef5994
krzemLech <krzemmo@gmail.com>
2023-11-23 22:58:44 +0100

    added all pages and metatags

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,9 +3,11 @@ import { defineCollection, z } from "astro:content";
 const postsCollection = defineCollection({
     schema: z.object({
         title: z.string(),
-        date: z.string(),
-        image: z.string(),
+        image: z.string().optional(),
         author: z.string(),
+        categories: z.array(z.string()),
+        date: z.string().transform((date) => new Date(date).toLocaleDateString("en-US", { year: "numeric", month: "long", day: "numeric" })),
+        featured: z.boolean().default(false),
     }),
 });
 
